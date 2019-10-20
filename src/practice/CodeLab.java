e815899431e0c399eb1f2671ffe6d5436de15759
arifovi22 <arifovi22@gmail.com>
2019-10-20 14:31:56 -0400

    java basic learning

---
+++ src/practice/CodeLab.java
@@ -0,0 +1,17 @@
+package practice;
+
+
+
+public class CodeLab {
+	 
+	
+	
+	public static void main(String[] args) {
+		String word ="m";
+	   System.out.println("Today's Word-Of-The-Day is:"+ word);
+			
+	}
+	
+}
+
+
