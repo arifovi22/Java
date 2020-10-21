cd6884a1cd79d62ebc48b35c646d6173c9a0b6c1
arifovi22 <arifovi22@gmail.com>
2020-10-20 21:12:58 -0400

    first

--- src/practice/CodeLab.java
+++ src/practice/CodeLab.java
@@ -3,15 +3,29 @@ package practice;
 
 
 public class CodeLab {
-	 
 	
 	
-	public static void main(String[] args) {
-		String word ="m";
-	   System.out.println("Today's Word-Of-The-Day is:"+ word);
-			
+	
+	public static void main(String[] a) {
+	    
+//	    int num1=2;
+//	    int num2=4;
+//	    int num3=6;
+//	    double avg ;
+//	    avg = (num1+num2+num3)/3;
+//	    System.out.println(avg);
+//	    
+//	    System.out.println(hexValue1);
+//	    System.out.println(hexValue2);
+//	    System.out.println(hexValue3);
+//	    System.out.println(hexValue4);
+		int v = 20; --v; System.out.println(v++);
+
+
+
+
 	}
-	
+
 }
 
 
